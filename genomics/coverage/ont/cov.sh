5f27d67ed15e4b4030915b2f6f9cf8f8dacc35b1
tobiasrausch <rauschtobi@gmail.com>
2022-01-19 13:34:30 +0100

    SVs

---
+++ genomics/coverage/ont/cov.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+export PATH=${BASEDIR}/../../conda/bin:${PATH}
+
+source activate variants
+
+HG=${BASEDIR}/../../genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+MAP=Homo_sapiens.GRCh38.dna.primary_assembly.fa.e1.r101.gz
+
+for BAM in ${BASEDIR}/../../alignment/ont/*.bam
+do
+    if [ -f ${BAM} ]
+    then
+	ID=`echo ${BAM} | sed 's/^.*\///' | sed 's/.bam$//'`
+	delly cnv -a -g ${HG} -m ${MAP} -c ${ID}.cov.gz -o ${ID}.bcf ${BAM}
+    fi
+done
