57dde495f4d91d4f9b945f9255b11bb41969549c
tobiasrausch <rauschtobi@gmail.com>
2022-01-20 09:07:38 +0100

    repeats

--- genomics/tithreads/repeats/rmsk.sh
+++ genomics/tithreads/repeats/rmsk.sh
@@ -8,6 +8,7 @@ do
     echo ${HG}
     wget http://hgdownload.cse.ucsc.edu/goldenpath/${HG}/database/rmsk.txt.gz
     zcat rmsk.txt.gz | awk -v OFS="\t" '{ print $6, $7, $8, $12, $11, $10 }' | sort -k1,1V -k2,2n | uniq | gzip -c > rmsk.${HG}.bed.gz
+    rm rmsk.txt.gz
 
     # Split by repeat class
     for REPEAT in `zcat rmsk.${HG}.bed.gz | cut -f 4 | grep -v "?" | sort | uniq`
