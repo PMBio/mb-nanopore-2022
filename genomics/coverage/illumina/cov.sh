d2b021c455b601c555f40d1ff58a392d3b7e9fc9
tobiasrausch <rauschtobi@gmail.com>
2022-01-26 09:23:11 +0100

    illumina read-depth

---
+++ genomics/coverage/illumina/cov.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+export PATH=${BASEDIR}/../../conda/bin:${PATH}
+
+source activate variants
+
+HG=${BASEDIR}/../../genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
+MAP=Homo_sapiens.GRCh38.dna.primary_assembly.fa.e1.r101.blacklist.gz
+
+for BAM in ${BASEDIR}/../../alignment/illumina/*.bam
+do
+    if [ -f ${BAM} ]
+    then
+	ID=`echo ${BAM} | sed 's/^.*\///' | sed 's/.bam$//'`
+	echo ${ID}
+	delly cnv -a -g ${HG} -m ${MAP} -c ${ID}.cov.gz -o ${ID}.bcf ${BAM}
+    fi
+done
