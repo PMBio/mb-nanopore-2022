2f47633939cb09f18effdbcb7f39465c85d2e36a
tobiasrausch <rauschtobi@gmail.com>
2022-02-03 16:36:44 +0100

    T2T assembly

--- genomics/genome/prepare_genome.sh
+++ genomics/genome/prepare_genome.sh
@@ -15,4 +15,8 @@ wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_refere
 samtools faidx GRCh38_full_analysis_set_plus_decoy_hla.fa
 bwa index GRCh38_full_analysis_set_plus_decoy_hla.fa
 
+# Telomere-to-telomere assembly (T2T)
+wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.draft_v1.1.fasta.gz
+gunzip chm13.draft_v1.1.fasta.gz
+samtools faidx chm13.draft_v1.1.fasta
 
