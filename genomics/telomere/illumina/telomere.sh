6dacab95cd098b9673b2d7073596ffc471d80d8d
tobiasrausch <rauschtobi@gmail.com>
2022-02-06 20:53:59 +0100

    short-read telomere motifs

--- genomics/telomere/illumina/telomere.sh
+++ genomics/telomere/illumina/telomere.sh
@@ -8,6 +8,7 @@ source activate variants
 
 HG=${BASEDIR}/../../genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
 
+# Find telomeric motifs
 for BAM in ${BASEDIR}/../../alignment/illumina/*.bam
 do
     if [ -f ${BAM} ]
@@ -15,4 +16,13 @@ do
 	ID=`echo ${BAM} | sed 's/^.*\///' | sed 's/.bam$//'`
 	/opt/dev/alfred/src/alfred telmotif -r ${HG} -o ${ID}.bed ${BAM}
     fi
+done
+
+# Filter
+for TYPE in tumor relapse
+do
+    echo ${TYPE}
+    cat ${TYPE}.bed  | grep -P "^chr[0-9X]*\t" | awk '$4>=5' > ${TYPE}.selected
+    bedtools intersect -a ${TYPE}.selected -b blood.bed -wao | awk '$5=="."' | cut -f 1-4
+    rm ${TYPE}.selected
 done
