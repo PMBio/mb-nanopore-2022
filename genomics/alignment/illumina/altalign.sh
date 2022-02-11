0fb5c79a12437f63316f1351b705620921459c9e
tobiasrausch <rauschtobi@gmail.com>
2022-02-11 11:38:18 +0100

    alt-aware mapping

--- genomics/alignment/illumina/altalign.sh
+++ genomics/alignment/illumina/altalign.sh
@@ -10,24 +10,29 @@ HG=${BASEDIR}/../../genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
 
 if [ -f ${HG} ]
 then
+    cd ${BASEDIR}
     if [ ! -d ${BASEDIR}/bwa.kit ]
     then
-	cd ${BASEDIR}
-	wget -O- http://sourceforge.net/projects/bio-bwa/files/bwakit/bwakit-0.7.15_x64-linux.tar.bz2/download > bwakit.tar
+	wget -O- http://sourceforge.net/projects/bio-bwa/files/bwakit/bwakit-0.7.12_x64-linux.tar.bz2/download > bwakit.tar
 	tar -xf bwakit.tar
+    fi
+    if [ ! -f hs38DH.fa ]
+    then
+	bwa.kit/run-gen-ref hs38DH
+	bwa.kit/bwa index hs38DH.fa
 	rm bwakit.tar
     fi
-    ${BASEDIR}/bwa.kit/run-bwamem -o blood -t 8 -HR"@RG\tID:blood\tSM:blood" ${HG} ${BASEDIR}/../../fastq/illumina/blood.1.fq.gz ${BASEDIR}/../../fastq/illumina/blood.2.fq.gz | sh
+    ${BASEDIR}/bwa.kit/run-bwamem -o blood -t 12 -R"@RG\tID:blood\tSM:blood" -H hs38DH.fa ${BASEDIR}/../../fastq/illumina/blood.1.fq.gz ${BASEDIR}/../../fastq/illumina/blood.2.fq.gz | sh
     samtools sort -o blood.bam blood.aln.bam
     samtools index blood.bam
     rm blood.aln.bam
     
-    ${BASEDIR}/bwa.kit/run-bwamem -o tumor -t 8 -HR"@RG\tID:tumor\tSM:tumor" ${HG} ${BASEDIR}/../../fastq/illumina/tumor.1.fq.gz ${BASEDIR}/../../fastq/illumina/tumor.2.fq.gz | sh
+    ${BASEDIR}/bwa.kit/run-bwamem -o tumor -t 12 -R"@RG\tID:tumor\tSM:tumor" -H hs38DH.fa ${BASEDIR}/../../fastq/illumina/tumor.1.fq.gz ${BASEDIR}/../../fastq/illumina/tumor.2.fq.gz | sh
     samtools sort -o tumor.bam tumor.aln.bam
     samtools index tumor.bam
     rm tumor.aln.bam
 
-    ${BASEDIR}/bwa.kit/run-bwamem -o relapse -t 8 -HR"@RG\tID:relapse\tSM:relapse" ${HG} ${BASEDIR}/../../fastq/illumina/relapse01.1.fq.gz ${BASEDIR}/../../fastq/illumina/relapse01.2.fq.gz | sh
+    ${BASEDIR}/bwa.kit/run-bwamem -o relapse -t 12 -R"@RG\tID:relapse\tSM:relapse" -H hs38DH.fa ${BASEDIR}/../../fastq/illumina/relapse01.1.fq.gz ${BASEDIR}/../../fastq/illumina/relapse01.2.fq.gz | sh
     samtools sort -o relapse.bam relapse.aln.bam
     samtools index relapse.bam
     rm relapse.aln.bam
