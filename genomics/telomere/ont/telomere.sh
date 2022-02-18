318b423ed68a08b5799ae1b563fc63772825bdb1
tobiasrausch <rauschtobi@gmail.com>
2022-02-18 11:33:44 +0100

    telomere reads

--- genomics/telomere/ont/telomere.sh
+++ genomics/telomere/ont/telomere.sh
@@ -7,6 +7,7 @@ export PATH=${BASEDIR}/../../conda/bin:${PATH}
 source activate telomere
 
 HG=${BASEDIR}/../../genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+CHM13=${BASEDIR}/../../genome/chm13.draft_v1.1.fasta
 
 for BAM in ${BASEDIR}/../../alignment/ont/*.bam
 do
@@ -20,5 +21,11 @@ do
 	zcat ${ID}.bed.gz  | tail -n +2 | cut -f 4 | sort | uniq > ${ID}.reads
 	# Extract reads
 	/opt/dev/lorax/src/lorax extract -g ${HG} -r ${ID}.reads -o ${ID}.match.gz -f ${ID}.fa.gz ${BAM}
+
+	# Map to CHM13
+	minimap2 -ax map-ont ${CHM13} ${ID}.fa.gz | samtools sort -o assembly.bam -
+	samtools index assembly.bam
+	/opt/dev/lorax/src/lorax extract -g ${CHM13} -r ${ID}.reads -o ${ID}.chm13.match.gz -f ${ID}.chm13.fa.gz assembly.bam
+	rm assembly.bam assembly.bam.bai
     fi
 done
