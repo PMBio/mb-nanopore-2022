cae221cc9cc4965bc45056489682f517a4cbb3bc
tobiasrausch <rauschtobi@gmail.com>
2022-07-07 17:18:32 +0200

    Templated insertion thread simulations

---
+++ genomics/sv/simulations/plot.R
@@ -0,0 +1,45 @@
+library(ggplot2)
+
+txtFontSize=16
+axisFontSize=16
+axisTtlFontSize=18
+lgdTtlFontSize=18
+lgdFontSize=16
+scienceTheme=theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), legend.key=element_blank(), legend.background=element_blank(), panel.background = element_blank(), panel.border=element_blank(), strip.background = element_blank(), axis.line=element_line(size=0.7, color="black"), axis.text.x=element_text(angle=45, size=axisFontSize, hjust=1), axis.text.y=element_text(size=axisFontSize), axis.title.x=element_text(size=axisTtlFontSize), axis.title.y=element_text(size=axisTtlFontSize), legend.title=element_text(size=lgdTtlFontSize, face="bold"), legend.text=element_text(size=lgdFontSize), text=element_text(size=txtFontSize), strip.text.x=element_text(size=axisTtlFontSize))
+
+
+dat = read.table("summary.stats.tsv", header=T)
+dat$coverage = factor(dat$coverage)
+
+# F1 score lines
+df = data.frame()
+for (f1 in c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)) {
+  for(p in (1:1000)/1000) {
+     r = f1 * p / (2 * p - f1)
+     if ((r >= 0) && (r <= 1)) {
+        df = rbind(df, data.frame(recall=r, precision=p, f1=f1))
+     }
+  }
+}
+df$f1 = factor(df$f1, levels=c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9))
+labels = df[df$precision == 1,]
+labels$text = paste0("F1=", labels$f1)
+labels$recall = labels$recall - 0.01
+
+p = ggplot(data=dat, aes(x=precision, y=recall))
+p = p + geom_point(aes(color=coverage))
+p = p + xlim(0,1) + ylim(0,1)
+p = p + geom_line(data=df, aes(x=precision, y=recall, group=f1), color="gray", linetype="dashed")
+p = p + geom_text(data=labels, aes(x=precision, y=recall, label=text), color="darkgray")
+p = p + xlab("Precision")
+p = p + ylab("Recall")
+p = p + labs(color="Coverage")
+p = p + scienceTheme
+p
+
+
+
+
+
+
+  
---
+++ genomics/sv/simulations/visor.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+export PATH=${BASEDIR}/../../conda/bin:${PATH}
+
+source activate sv
+
+HG=${BASEDIR}/../../genome/hg38.fa
+NTITHREADS=10
+NSOURCES=3
+NSEGMENTS=100
+
+# Simulate using chr18
+samtools faidx ../../genome/hg38.fa chr18 > chr18.fa
+samtools faidx chr18.fa
+
+# Benchmark SV calling
+echo -e "svtype\tmode\tct\tcoverage\treadlen\trecall\tprecision\tf1\tgtconc\tprecise"
+SVT=TITHREAD
+for COV in 5 10 20 30 40
+do
+    #pb ont
+    for MODE in ill 
+    do
+	LEN=150
+	if [ ${MODE} == "ont" ]
+	then
+	    MODEL="nanopore2018"
+	    RT="nanopore"
+	    LEN=15000
+	fi
+	if [ ${MODE} == "pb" ]
+	then
+	    MODEL="pacbio2016"
+	    RT="pacbio"
+	    LEN=15000
+	fi
+	# Simulate anew
+	if [ ! -d sim_svt${SVT}_${MODE}_cov${COV}_len${LEN} ]
+	then
+	    mkdir sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}
+
+	    # Control
+	    cat chr18.fa.fai  | awk '{print $1"\t1\t"$2"\t100.0\t100.0";}' > sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/simulate.bed
+	    touch sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.hap1.bed sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.hap2.bed
+	    VISOR HACk -g chr18.fa -b sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.hap1.bed sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.hap2.bed -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/hack_control
+	    
+	    # Simulate haplotypes
+	    for J in `seq 1 1 ${NTITHREADS}`
+	    do
+		rm -f templates.fa
+		for I in `seq 1 1 ${NSOURCES}` 
+		do
+		    ST1=`echo $(($RANDOM%30000)) | awk '{print ($1*1000 + 1000000);}'`
+		    SIZE=`echo $(($RANDOM%500)) | awk '{print ($1 + 250);}'`
+		    ED1=`expr ${ST1} + ${SIZE}`
+		    SEQUENCE=`samtools faidx chr18.fa chr18:${ST1}-${ED1} | tail -n +2 | tr '\n' '#' | sed 's/#//g'`
+		    echo -e "chr18\t${ST1}\t${ED1}\tSegment${I}\tThread${J}" >> sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.bed
+		    echo ${SEQUENCE} >> templates.fa
+		done
+		rm -f tithread.fa
+		for I in `seq 1 1 ${NSEGMENTS}`
+		do
+		    CHOICE=`echo $(($RANDOM%${NSOURCES})) | awk '{print ($1 + 1);}'`
+		    FLIP=`echo $(($RANDOM%2))`
+		    if [ ${FLIP} -eq 1 ]
+		    then
+			sed "${CHOICE}q;d" templates.fa | tr 'acgtACGT' 'tgcaTGCA' | rev >> tithread.fa
+		    else
+			sed "${CHOICE}q;d" templates.fa >> tithread.fa
+		    fi
+		done
+		ST=`echo $(($RANDOM%30000)) | awk '{print ($1*1000 + 50000000);}'`
+		ED=`expr ${ST} + 1`
+		SEQUENCE=`cat tithread.fa | tr '\n' '#' | sed 's/#//g'`
+		rm -f templates.fa tithread.fa
+		echo -e "chr18\t${ST}\t${ED}\tinsertion\t${SEQUENCE}\t0" >> sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.hap1.bed
+	    done
+	    VISOR HACk -g chr18.fa -b sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.hap1.bed sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.hap2.bed -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/hack_tumor
+	    # Draw reads
+	    if [ ${MODE} == "ill" ]
+	    then
+		VISOR SHORtS --coverage ${COV} --length ${LEN} -g chr18.fa -s sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/hack_tumor/ -b sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/simulate.bed -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/tumor
+		VISOR SHORtS --coverage ${COV} --length ${LEN} -g chr18.fa -s sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/hack_control/ -b sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/simulate.bed -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/control
+	    else
+		VISOR LASeR --coverage ${COV} --length_mean ${LEN} --error_model ${MODEL} --qscore_model ${MODEL} --read_type ${RT} -g chr18.fa -s sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/hack_tumor/ -b sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/simulate.bed -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/tumor
+		VISOR LASeR --coverage ${COV} --length_mean ${LEN} --error_model ${MODEL} --qscore_model ${MODEL} --read_type ${RT} -g chr18.fa -s sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/hack_control/ -b sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/simulate.bed -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/control  
+	    fi
+	fi
+	# Call SVs
+	if [ ! -f sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/results.bed ]
+	then
+	    if [ ${MODE} == "ill" ]
+	    then
+		/opt/dev/rayas/bin/rayas call -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/results.bed -g chr18.fa -m sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/control/sim.srt.bam sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/tumor/sim.srt.bam
+		cut -f 4,9 sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/results.bed | sed -e '1s/^/graph {\n/' | sed -e '$a}' > sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/results.dot
+		dot -Tpdf sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/results.dot -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/results.pdf
+	    else
+		echo ${MODE}
+		#/opt/dev/delly/bin/delly lr -y ${MODE} -o sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/${SVT}.bcf -g data/chr18.fa sim_svt${SVT}_${MODE}_cov${COV}_len${LEN}/out/sim.srt.bam
+	    fi
+	fi
+    done
+done
