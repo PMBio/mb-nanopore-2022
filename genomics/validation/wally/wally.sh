84809a6d9afd733d11d4b9b6e9085a6f31515758
tobiasrausch <rauschtobi@gmail.com>
2022-08-01 13:51:24 +0200

    dotplots

---
+++ genomics/validation/wally/wally.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+export PATH=${BASEDIR}/../../conda/bin:${PATH}
+
+source activate visualization
+
+HG=${BASEDIR}/../../genome/hg38.fa
+if [ -f ${HG} ]
+then
+    cd ${BASEDIR}
+    for ALIGN in ../alignment/ont/*.bam
+    do
+	if [ -f ${ALIGN} ]
+	then
+	    ID=`echo ${ALIGN} | sed 's/^.*\///' | sed 's/H021-//' | sed 's/-.*$//'`
+	    echo ${ID}
+	    if [ ${ID} == "D7K5QE" ]
+	    then
+		samtools view ${ALIGN} chr12:103700218-103700284 | cut -f 1 | sort | uniq -d > reads
+	    else
+		samtools view ${ALIGN} chr16:63892950-63893800 chr16:57105500-57105800 | cut -f 1 | sort | uniq -d > reads
+	    fi
+	    /opt/dev/wally/src/wally dotplot -g ${HG} -R reads -s 13000 ${ALIGN}
+	    rm reads
+	fi
+    done
+fi
