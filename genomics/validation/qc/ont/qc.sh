6ff6dd1451036f560db3adf7d155a7889001a08b
tobiasrausch <rauschtobi@gmail.com>
2022-08-01 12:05:25 +0200

    qc

---
+++ genomics/validation/qc/ont/qc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
+export PATH=${BASEDIR}/../../../conda/bin:${PATH}
+
+source activate align
+
+HG=${BASEDIR}/../../../genome/hg38.fa
+
+for BAM in ${BASEDIR}/../../alignment/ont/*.bam
+do
+    if [ -f ${BAM} ]
+    then
+	ID=`echo ${BAM} | sed 's/^.*\///' | sed 's/.bam$//'`
+	alfred qc -r ${HG} -j ${ID}.json.gz -o ${ID}.tsv.gz ${BAM}
+    fi
+done
