4ba9ceec83208914e4c05da6a7e12d2c271c68f7
Tobias Rausch <rauschtobi@gmail.com>
2022-09-26 16:52:14 +0200

    ti threads

--- genomics/tithreads/ont/tithreads.sh
+++ genomics/tithreads/ont/tithreads.sh
@@ -15,13 +15,13 @@ do
 	ID=`echo ${BAM} | sed 's/^.*\///' | sed 's/.bam$//'`
 	if [ ${ID} == "Germline" ]; then continue; fi
 	echo ${ID}
-	/usr/bin/time -v /opt/dev/lorax/src/lorax tithreads -o ${ID} -g ${HG} -m ${BASEDIR}/../../alignment/ont/Germline.bam ${BAM} 
-	/usr/bin/time -v /opt/dev/lorax/src/lorax extract -a -g ${HG} -o ${ID}.match.gz -f ${ID}.fa.gz -r ${ID}.reads ${BAM}
+	lorax tithreads -o ${ID} -g ${HG} -m ${BASEDIR}/../../alignment/ont/Germline.bam ${BAM} 
+	lorax extract -a -g ${HG} -o ${ID}.match.gz -f ${ID}.fa.gz -r ${ID}.reads ${BAM}
 	for CLUSTER in `tail -n +2 ${ID}.reads | cut -f 3 | sort | uniq`
 	do
 	    echo ${ID} ${CLUSTER}
 	    zcat ${ID}.fa.gz | grep --no-group-separator -A 1 -w -Ff <(awk '$3=="'${CLUSTER}'"' ${ID}.reads | cut -f 1 | sort | uniq) > input.fasta
-	    /usr/bin/time -v ${BASEDIR}/../../assembly/shasta-Linux-0.10.0 --input input.fasta --config Nanopore-May2022
+	    ${BASEDIR}/../../assembly/shasta-Linux-0.10.0 --input input.fasta --config Nanopore-May2022
 	    cp ShastaRun/Assembly.fasta ${ID}.assembly.${CLUSTER}.fasta
 	    rm -rf ShastaRun/ input.fasta
 	    if [ -f ${ID}.assembly.${CLUSTER}.fasta ]
@@ -31,7 +31,7 @@ do
 
 		# Visualize contigs
 		#samtools view ${ID}.assembly.${CLUSTER}.bam | cut -f 1 | sort | uniq > reads
-		#/opt/dev/wally/src/wally dotplot -a -g ${HG} -R reads ${ID}.assembly.${CLUSTER}.bam
+		#wally dotplot -a -g ${HG} -R reads ${ID}.assembly.${CLUSTER}.bam
 	    fi
 	done
     fi
