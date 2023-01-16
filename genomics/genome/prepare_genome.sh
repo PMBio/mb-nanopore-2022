d874ef17cc774a2350fbb9359953151d5f81ee07
Madalina Giurgiu <Madalina.Giurgiu@charite.de>
2023-01-16 14:25:32 +0100

    add reference hs37d5_mm10 for circleseq data

--- genomics/genome/prepare_genome.sh
+++ genomics/genome/prepare_genome.sh
@@ -16,7 +16,15 @@ zcat chm13v2.0_maskedY_rCRS.fa.gz > t2t.fa
 samtools faidx t2t.fa
 rm chm13v2.0_maskedY_rCRS.fa.gz
 
+# Circle-seq reference genome
+wget ftp://ftp.1000genomes.ebi.ac.uk//vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
+gunzip -c hs37d5.fa.gz > hs37d5_mm10.fa
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M22/GRCm38.p6.genome.fa.gz
+gunzip -c GRCm38.p6.genome.fa.gz > mm10.fa &&  sed -i -e 's/>/>m\./g' mm10.fa && cat mm10.fa >> hs37d5_mm10.fa
+rm hs37d5.fa.gz GRCm38.p6.genome.fa.gz mm10.fa
+
 # Index
 bwa index hg38.fa
 bwa index t2t.fa
+bwa index hs37d5_mm10.fa
 
